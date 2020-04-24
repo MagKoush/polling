6fcee094fdf5122122dc1f64dfe5c210c717c6cb
Arash Koushkebaghi <arash.koush@gmail.com>
2020-04-23 22:29:51 -0700

    server only set up the middlewares

--- src/server.ts
+++ src/server.ts
@@ -2,30 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import "dotenv-defaults/config";
 import mongoose from 'mongoose';
-import { Poll } from './mongo/models';
 import { MONGO_URL, PORT } from './constants';
+import { Polls, Users, Votes, Questions } from './routes';
 
 const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use('/questions', Questions);
+app.use('/polls', Polls);
+app.use('/votes', Votes);
+app.use('/users', Users);
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.post('/polls', async (req, res) => {
-  try {
-    const { body: { name, type, creator, timelimit, created, ended } } = req;
-    const poll = new Poll({ name, type, creator, timelimit, created, ended });
-
-    await poll.save();
-    res.status(200).send(`Poll Added: ${poll}`);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-})
+app.get('/', (req, res) => res.send('Hello RamAsh! 🍻🍻'));
 
 app.listen(PORT, async () => {
   try {
     console.info(`App is Running at PORT: ${PORT}!`);
-    await mongoose.connect(MONGO_URL ? MONGO_URL : '', { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(MONGO_URL ? MONGO_URL : '', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, });
     const db = mongoose.connection;
     console.info('MongoDb is connected!')
 
