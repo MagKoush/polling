2913266ddc7057830ee8d549709258b8e524f183
arashk70 <arash.koush@gmail.com>
2020-04-30 19:48:15 -0700

    define routes in your server

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
 import 'dotenv-defaults/config';
 
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
 
 import { MONGO_URL, PORT } from './constants';
-import { Polls, Questions, Users, Votes } from './routes';
+import { Elections, Polls, Users, Votes } from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //Routes
-app.use('/questions', Questions);
+app.use('/elections', Elections);
 app.use('/polls', Polls);
 app.use('/votes', Votes);
 app.use('/users', Users);
