b893e98278c08c4395109bfc423cf0795a9189d2
arashk70 <arash.koush@gmail.com>
2020-05-02 19:49:53 -0700

    add passport middlware

--- src/server.ts
+++ src/server.ts
@@ -3,24 +3,50 @@ import 'dotenv-defaults/config';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import session from 'express-session';
 import mongoose from 'mongoose';
+import passport from 'passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
-import { MONGO_URL, PORT } from './constants';
-import { Elections, Polls, Users, Votes } from './routes';
+import { MONGO_URL, PASSPORT_SECRET, PORT } from './constants';
+import { User } from './mongo';
+import { Auth, Elections, Polls, Users, Votes } from './routes';
 
 const app = express();
 
+// Cors
 app.use(cors());
+
+// BodyParser
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+// Passport
+app.use(session({ cookie: { secure: true }, resave: false, saveUninitialized: true, secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new Strategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: PASSPORT_SECRET,
+    },
+    async ({ _id }, done) => {
+      const user = await User.findById(_id);
+      return done(null, user);
+    },
+  ),
+);
+
 //Routes
+app.use('/auth', Auth);
 app.use('/elections', Elections);
 app.use('/polls', Polls);
 app.use('/votes', Votes);
 app.use('/users', Users);
 
-app.get('/', (_req, res) => res.send('Hello RamAsh! 🍻🍻'));
+app.get('/', passport.authenticate('jwt', { session: false }), (req, res) => res.send('Hello MagKoush! 🍻🍻'));
 
 app.listen(PORT, async () => {
   try {
